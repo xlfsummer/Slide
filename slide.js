263b366262973ba0ef26cd3d5a1d7ec078d1450c
xlfsummer <xlf-summer@qq.com>
2016-08-08 00:07:16 +0800

    增加基本测试页面，增加，滑动效果，增加基本切换逻辑

--- slide.js
+++ slide.js
@@ -1,22 +1,33 @@
-function Slide(element, option){
-	function examinType(){
-		if(this instanceof Slide){
-			return this;
-		}
-		throw new Error("Slide 函数只能用作构造函数!");
-	}
-	
+function Slide(element, option) {
+	// 私有字段
+
+	//当前
+	var _currentIndex = 1;
+
 	Object.defineProperties(this,{
-		slideElement:{
+		slideBox:{
 			value: element,
 		},
-		currentIndex:{
-			value: 1,
-			writable: true,
+		currentIndex: {
+			set: function (index) {
+				if (index >= 1 && index <= this.pageCount) {
+					_currentIndex = index;
+				} else {
+					throw new Error("currentIndex 的值不在正确范围内");
+				}
+			},
+			get: function () {
+				return _currentIndex;
+			}
+		},
+		currentItem: {
+			get: function () {
+				return this.slideBox.children[this.currentIndex - 1];
+			}
 		},
 		pageCount:{
 			get: function(){
-				return this.slideElement.childElementCount;
+				return this.slideBox.childElementCount;
 			}
 		},
 		nextIndex:{
@@ -28,9 +39,23 @@ function Slide(element, option){
 			get: function(){
 				return this.currentIndex - 1 < 1 ? this.pageCount : this.currentIndex -1;
 			}
+		},
+		effect: {
+			value: (function () {
+				var effect = option && option.effect || "left";
+				effect = effect.toLowerCase();
+				return effect;
+			})(),
 		}
-		
 	});
+
+	(function examinType(that) {
+		if (that instanceof Slide) {
+			return that;
+		} else {
+			throw new Error("Slide 函数只能用作构造函数!");
+		}
+	})(this);
 
 	// Object.defineProperty(this,"nextIndex",{
 	// 	get : function(){
@@ -44,8 +69,24 @@ function Slide(element, option){
 //SlideTo 切换到第 index 张轮播 
 Slide.prototype.slideTo = function(index){
 	//逻辑切换
+	this.currentItem.classList.remove("active");
+	this.currentIndex = index;
+	this.currentItem.classList.add("active");
+
 	//执行动画
+	this.animateSlidebox();
 }
 
-Slide.prototype.slideNext = function(){
+Slide.prototype.slideNext = function () {
+	this.slideTo(this.nextIndex)
+}
+Slide.prototype.slidePrev = function () {
+	this.slideTo(this.prevIndex)
+}
+
+Slide.prototype.animateSlidebox = function(){
+	if (this.effect == "left") {
+		this.slideBox.style.transition = "0.5s";
+		this.slideBox.style.transform = "translateX(-" + (this.currentIndex - 1) * this.slideBox.children[0].offsetWidth + "px)";
+	}	
 }
