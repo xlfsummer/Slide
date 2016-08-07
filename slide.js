e6691ee42ebb1033c181aa12bcf05a994ca1c60f
xlfsummer <xlf-summer@qq.com>
2016-08-07 16:13:12 +0800

    向 Slides 的实例增加了 currentIndex，pageCount 等等的基础属性

--- slide.js
+++ slide.js
@@ -1,18 +1,51 @@
-function Slide(element,option){
+function Slide(element, option){
 	function examinType(){
 		if(this instanceof Slide){
 			return this;
 		}
 		throw new Error("Slide 函数只能用作构造函数!");
 	}
-	this.currentIndex = 1;
 	
-	Object.defineProperty(this,"length");
+	Object.defineProperties(this,{
+		slideElement:{
+			value: element,
+		},
+		currentIndex:{
+			value: 1,
+			writable: true,
+		},
+		pageCount:{
+			get: function(){
+				return this.slideElement.childElementCount;
+			}
+		},
+		nextIndex:{
+			get: function(){
+				return this.currentIndex + 1 > this.pageCount ? 1 : this.currentIndex + 1;
+			}
+		},
+		prevIndex:{
+			get: function(){
+				return this.currentIndex - 1 < 1 ? this.pageCount : this.currentIndex -1;
+			}
+		}
+		
+	});
+
+	// Object.defineProperty(this,"nextIndex",{
+	// 	get : function(){
+	// 		return this.currentIndex + 1;
+	// 	}
+	// });
 }
 
 //Slide 的方法
 
 //SlideTo 切换到第 index 张轮播 
-Slide.prototype.slideTo(index){
-	
+Slide.prototype.slideTo = function(index){
+	//逻辑切换
+	//执行动画
+}
+
+Slide.prototype.slideNext = function(){
 }
